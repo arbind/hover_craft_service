827b1e817dae152d359e80b7b6cec5d2292cd81c
Arbind <arbind@carbonfive.com>
2013-07-06 21:43:23 -0700

    use AuthorizedUsers as a service

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,12 @@ module HoverCraftService
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # load any class extentions right now
+    Dir[Rails.root.join('app', 'extentions', "*.rb")].each {|l| require l }
+    # Custom directories to be autoloadable.
+    config.autoload_paths += Dir[Rails.root.join('app', 'mixins', '{**}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'services', '{**/*}')]
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
