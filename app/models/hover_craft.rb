73aea12eaf122ddfc7b35652fdd031147606d299
Arbind <arbind@carbonfive.com>
2013-08-04 18:47:18 -0700

    define some usefull scopes on HoverCraft

--- app/models/hover_craft.rb
+++ app/models/hover_craft.rb
@@ -2,6 +2,15 @@ class HoverCraft
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  # score constants
+  FIT_duplicate_crafts = -2 # flagen 2               hover crafts bind to the same already existing craft
+  FIT_need_to_explore = -1  # flag go               get hover crafts and explore this
+  FIT_zero = 0              # its not a fit
+  FIT_missing_craft = 1     # missing info
+  FIT_check_manually = 3    # flagr ma              nual check
+  FIT_neutral = 5           # at least its not a bad fit
+  FIT_absolute = 8          # known to be a fit
+
   field :craft_id
 
   field :tweet_streamer_id
@@ -36,46 +45,59 @@ class HoverCraft
   field :website_profile      , type: Hash
   field :website_craft        , type: Boolean
 
-  scope :with_yelp_craft,     exists(yelp_id: true)
-  scope :with_twitter_craft,  exists(twitter_id: true)
-  scope :with_website_craft,  exists(website_id: true)
-  scope :with_facebook_craft, exists(facebook_id: true)
-  scope :with_tweet_streamer, exists(tweet_streamer_id: true)
+  # manual overrides
+  field :approve_this         , type: Boolean
+  field :flag_this            , type: Boolean # Needs follow up for corrections
+  field :skip_this            , type: Boolean
 
-  scope :without_yelp_craft,     exists(yelp_id: false)
-  scope :without_twitter_craft,  exists(twitter_id: false)
-  scope :without_website_craft,  exists(website_id: false)
-  scope :without_facebook_craft, exists(facebook_id: false)
-  scope :without_tweet_streamer, exists(tweet_streamer_id: false)
+  field :fit_score            , type: Integer, default: FIT_check_manually
+  field :fit_score_name       , type: Integer, default: FIT_check_manually
+  field :fit_score_website    , type: Integer, default: FIT_check_manually
+  field :fit_score_username   , type: Integer, default: FIT_check_manually
+  # # field :fit_score_food, type: Integer, default: FIT_check_manually
+  # # field :fit_score_mobile, type: Integer, default: FIT_check_manually
+
+  scope :crafted          , exists(craft_id: true).desc(:fit_score)
+  scope :uncrafted        , exists(craft_id: false).desc(:fit_score)
+
+  scope :approved         , where(approve_this: true)
+  scope :flagged          , where(flag_this: true)
+  scope :skipped          , where(skip_this: true)
+  scope :unapproved       , excludes(approve_this: true)
+  scope :unflagged        , excludes(flag_this: true)
+  scope :unskipped        , excludes(skip_this: true)
+
+  scope :with_yelp        , exists(yelp_id: true)
+  scope :with_twitter     , exists(twitter_id: true)
+  scope :with_website     , exists(website_url: true)
+  scope :with_facebook    , exists(facebook_id: true)
+  scope :with_streamer    , exists(tweet_streamer_id: true)
+
+  scope :without_yelp     , exists(yelp_id: false)
+  scope :without_twitter  , exists(twitter_id: false)
+  scope :without_website  , exists(website_url: false)
+  scope :without_facebook , exists(facebook_id: false)
+  scope :without_streamer , exists(tweet_streamer_id: false)
+
+  # crafts with both twitter and yelp
+  scope :twelps,  exists(yelp_id: true).exists(twitter_id: true).desc(:yelp_name)
+
+  scope :with_missing_web_craft, any_of( {yelp_id: nil},
+                                     {twitter_id: nil},
+                                     {facebook_id: nil},
+                                     {website_url: nil}
+                                   ).desc(:yelp_name)
+
+  # score scopes
+  scope :need_to_explore, where(fit_score: FIT_need_to_explore)
+  scope :check_manually,  where(fit_score: FIT_check_manually)
+  scope :missing_craft,   where(fit_score: FIT_missing_craft)
+  scope :zero_fit,        where(fit_score: FIT_zero)
+  scope :neutral_fit,     where(fit_score: FIT_neutral)
+  scope :absolute_fit,    where(fit_score: FIT_absolute)
 
   def tweet_streamer
     TweetStreamer.where(id:tweet_streamer_id).first
   end
 
 end
-
-  # # score
-  # field :fit_score          , type: Integer, default: FIT_check_manually
-  # field :fit_score_name     , type: Integer, default: FIT_check_manually
-  # field :fit_score_website  , type: Integer, default: FIT_check_manually
-  # field :fit_score_username , type: Integer, default: FIT_check_manually
-  # # field :fit_score_food, type: Integer, default: FIT_check_manually
-  # # field :fit_score_mobile, type: Integer, default: FIT_check_manually
-
-  # # Needs follow up for corrections
-  # field :flag                 , type: Boolean
-
-  # # manual overrides
-  # field :skip_this_craft      , type: Boolean, default: false
-  # field :approve_this_craft   , type: Boolean, default: false
-
-  # # error states
-  # field :error_duplicate_crafts, type: Boolean, default: false
-
-  #  # scopes
-  # scope :need_to_explore, where(fit_score: FIT_need_to_explore)
-  # scope :check_manually,  where(fit_score: FIT_check_manually)
-  # scope :missing_craft,   where(fit_score: FIT_missing_craft)
-  # scope :zero_fit,        where(fit_score: FIT_zero)
-  # scope :neutral_fit,     where(fit_score: FIT_neutral)
-  # scope :absolute_fit,    where(fit_score: FIT_absolute)
