e7184f1fb1b7a56368fc913cef8be8cdce398762
Arbind <arbind@carbonfive.com>
2013-08-17 12:24:00 -0700

    add placeholders

---
+++ spec/services/workers/facebook_handler/populate_facebook_craft.rb
@@ -0,0 +1,14 @@
+describe :populate_populate_craft do
+  # it 'schedules a populate website craft if no facebook craft could be found no website_craft exists'
+  # it 'populates the facebook_craft for a hover craft'
+  # it 'schedules a ResolveUrl job for the facebook_website_url'
+  # it 'identifies a potential facebook_craft for a hover craft'
+  # context 'given an identified twitter craft' do
+  #   it 'sets the twitter_screen_name on the hover craft'
+  #   it 'schedules a TwitterCraftCreate'
+  # end
+  # context 'given no potential' do
+  #   it 'sets the twitter_id to empty string'
+  #   it 'does not schedule a TwitterCraftCreate'
+  # end
+end
