e237b530cd3bd6a2f3aff971000765d69a1cfa99
Arbind <arbind@carbonfive.com>
2013-08-21 00:11:24 -0700

    :populate_website_craft

--- app/services/workers/website_handler.rb
+++ app/services/workers/website_handler.rb
@@ -1,22 +1,26 @@
 class WebsiteHandler
 
   def self.populate_website_craft(hover_craft)
+    return if hover_craft.website_craft or hover_craft.website_url
     website_url = identify_website_url hover_craft
+    return if website_url.to_s.empty?
     website_craft = WebsiteApi.website_info website_url
     if website_craft
       hover_craft.update_attributes(website_craft.to_hover_craft)
-      WorkLauncher.launch :populate_hover_craft, hover_craft if hover_craft.web_craft_missing?
+      WorkLauncher.launch :populate_hover_craft, hover_craft
     end
-
   end
 
   private
 
   def self.identify_website_url(hover_craft)
     candidate_urls = []
-    if hover_craft.twitter_website_url and hover_craft.twitter_name and hover_craft.tweet_streamer.present?
+    if hover_craft.twitter_website_url and hover_craft.twitter_name
       candidate_urls <<  hover_craft.twitter_website_url
-    elsif hover_craft.twitter_website_url and hover_craft.twitter_name
+    elsif hover_craft.facebook_website_url
+      candidate_urls <<  hover_craft.facebook_website_url
+    elsif hover_craft.yelp_website_url
+      candidate_urls <<  hover_craft.yelp_website_url
       candidate_urls <<  hover_craft.twitter_website_url
     end
     candidate_urls.reject!{ |url| url.nil? or Web.provider_href? url }
--- spec/services/workers/website_handler/populate_website_craft_spec.rb
+++ spec/services/workers/website_handler/populate_website_craft_spec.rb
@@ -1,30 +1,44 @@
 require 'spec_helper'
 require 'sidekiq/testing'
 describe :populate_website_craft do
+  let!(:mock_web)           { Web.any_instance.stub(:document).and_return(mock_document) }
+  let!(:mock_final_url_for) { Web.stub(:final_url_for).and_return(final_url)}
+  let (:final_url)          { 'http://my-site.com' }
+  let (:title)              { 'My Biz' }
+  let (:yelp_href)          { 'http://yelp.com/biz/my-biz'}
+  let (:twitter_href)       { 'http://twitter.com/my_biz'}
+  let (:facebook_href)      { 'http://facebook.com/my.biz'}
+  let (:mock_document)      { Nokogiri::HTML "
+    <html>
+      <head>
+      <title>#{title}</title>
+      </head>
+      <body>
+        <h1>#{title}</h1>
+        <a href='#{yelp_href}'>yelper</a>
+        <a href='#{twitter_href}'>t</a>
+        <a href='#{facebook_href}'>fb</a>
+      </body>
+    </html>
+  "}
 
   shared_examples_for 'it found a website craft' do
     it 'populates the website_craft' do
-      expect{
-        WebsiteHandler.populate_website_craft(hover_craft)
-      }.to change(hover_craft, :website_url).to(final_url)
+      WebsiteHandler.populate_website_craft(hover_craft)
+      expect(hover_craft.website_url).to eq final_url
+      expect(hover_craft.website_name).to eq title
     end
     it 'finds links to yelp, facebook and twitter on the website' do
-      expect(true).to be_false
+      WebsiteHandler.populate_website_craft(hover_craft)
+      website_profile = hover_craft.website_profile
+      expect(website_profile[:yelp_links]).to eq [yelp_href]
+      expect(website_profile[:twitter_links]).to eq [twitter_href]
+      expect(website_profile[:facebook_links]).to eq [facebook_href]
     end
-    context 'given missing web crafts' do
-      it 'schedules :populate_hover_craft to fill in any missing web crafts' do
-        expect {
-          WebsiteHandler.populate_website_craft(hover_craft)
-        }.to change(PopulateHoverCraft.jobs, :size).by(1)
-      end
-    end
-    context 'given no missing web crafts' do
-      let (:hover_craft) { create :no_website_hover_craft}
-      it 'does not schedule :populate_hover_craft' do
-        expect {
-          WebsiteHandler.populate_website_craft(hover_craft)
-        }.to change(PopulateHoverCraft.jobs, :size).by(0)
-      end
+    it 'schedules :populate_hover_craft' do
+      expect {
+        WebsiteHandler.populate_website_craft(hover_craft)
+      }.to change(PopulateHoverCraft.jobs, :size).by(1)
     end
   end
 
@@ -41,50 +55,45 @@ describe :populate_website_craft do
     end
   end
 
-  let (:final_url) { 'http://my-site.com' }
-  let!(:double)      { Web.double(:final_url_for).and_return(final_url)}
-
   context 'given a website craft already exists' do
-    let (:hover_craft)  { create :hover_craft, :streamer, :twitter, :yelp }
+    let (:hover_craft)  { build :complete_hover_craft}
     it_behaves_like "it doesn't change anything"
   end
 
   context 'given a website craft was already crafted' do
-    let (:hover_craft)  { create :hover_craft, :streamer, :twitter, yelp_craft: true }
+    let (:hover_craft)  { build :no_website_hover_craft, website_craft: true }
     it_behaves_like "it doesn't change anything"
   end
 
-  context 'given a streamer and a twitter_website_url' do
-    let (:hover_craft)  { create :hover_craft, :streamer, :twitter, twitter_website_url: final_url  }
-    context 'for a valid website' do
+  context 'given a streamer and a twitter_craft' do
+    let (:final_url)    { hover_craft.twitter_website_url }
+    let (:hover_craft)  { build :hover_craft, :streamer, :twitter }
+    context 'with a valid twitter_website_url' do
       it_behaves_like 'it found a website craft'
+      it 'sets the website_url to the twitter_website_url' do
+        WebsiteHandler.populate_website_craft(hover_craft)
+        expect(hover_craft.website_url).to eq hover_craft.twitter_website_url
+      end
     end
-    context 'for an invalid website' do
-      let (:final_url) { "" }
+    context 'with an invalid twitter_website_url' do
+      let (:hover_craft)  { create :hover_craft, :streamer, :twitter, twitter_website_url: "" }
       it_behaves_like "it doesn't change anything"
     end
   end
 
-  context 'given a facebook_website_url' do
-    let (:hover_craft)  { create :hover_craft, :facebook, facebook_website_url: final_url  }
-    context 'for a valid website' do
+  context 'given a yelp_craft but no twitter_craft' do
+    let (:final_url)    { hover_craft.yelp_website_url }
+    let (:hover_craft)  { build :hover_craft, :yelp }
+    context 'with a valid yelp_website_url' do
       it_behaves_like 'it found a website craft'
+      it 'sets the website_url to the yelp_website_url' do
+        WebsiteHandler.populate_website_craft(hover_craft)
+        expect(hover_craft.website_url).to eq hover_craft.yelp_website_url
+      end
     end
-    context 'for an invalid website' do
-      let (:final_url) { "" }
+    context 'with an invalid yelp_website_url' do
+      let (:hover_craft)  { build :hover_craft, :yelp, yelp_website_url: "" }
       it_behaves_like "it doesn't change anything"
     end
   end
-
-  context 'given a yelp_website_url' do
-    let (:hover_craft)  { create :hover_craft, :yelp, yelp_website_url: final_url  }
-    context 'for a valid website' do
-      it_behaves_like 'it found a website craft'
-    end
-    context 'for an invalid website' do
-      let (:final_url) { "" }
-      it_behaves_like "it doesn't change anything"
-    end
-  end
-
 end
