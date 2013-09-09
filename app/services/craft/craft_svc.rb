2c91e4d8ec7ecff119790a7c1cf534eab29c2a79
Arbind <arbind@carbonfive.com>
2013-09-08 22:50:53 -0700

    use CraftSvc.materialize instead of create and update

--- app/services/craft/craft_svc.rb
+++ app/services/craft/craft_svc.rb
@@ -1,27 +1,13 @@
 class CraftSvc
 
   def self.materialize(craft)
-    if craft[:id]
-      updated_craft = update craft
-    else
-      updated_craft = create craft
-    end
-    updated_craft
+    response = RestClient.post materialize_craft_url, craft: craft
+    craft_from_response response
+  rescue => ex
+    raise ex
   end
 
   private
-
-  def self.create(craft)
-    url = create_craft_url
-    response = RestClient.post url, craft: craft
-    craft_from_response response
-  end
-
-  def self.update(craft)
-    url = update_craft_url craft[:id]
-    response =  RestClient.patch url, craft: craft
-    craft_from_response response
-  end
 
   def self.craft_from_response(response)
     return HashObject.new if response.blank?
@@ -30,11 +16,7 @@ class CraftSvc
     HashObject.new craft
   end
 
-  def self.create_craft_url
-    "#{CRAFT_SERVICE_ENDPOINT}/crafts"
+  def self.materialize_craft_url
+    "#{CRAFT_SERVICE_ENDPOINT}/materialize"
   end
-  def self.update_craft_url(craft_id)
-    "#{CRAFT_SERVICE_ENDPOINT}/crafts/#{craft_id}"
-  end
-
 end
