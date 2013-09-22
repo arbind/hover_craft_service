e89a65b5fd9197746f1d89128a5eacbe056b8fc6
Arbind <arbind@carbonfive.com>
2013-09-21 20:18:27 -0700

    don't initialize authorized users when in rake task

--- config/initializers/3_authorized_users.rb
+++ config/initializers/3_authorized_users.rb
@@ -1,5 +1,10 @@
-# Add the twitter ids for any users you want to authorize
-AuthorizedUsers.service.authorize({
-  '14956791' => 'Arbind Thakur',
-  # 'faeid???' => 'Fae ??',
-})
+unless $PROGRAM_NAME.end_with?('rake')
+  # Skip this initialization durring rake tasks
+  # Heroku runs rake asset:precompile in sandbox mode with no ENV or DBs
+
+  # Add the twitter ids for any users you want to authorize
+  AuthorizedUsers.service.authorize({
+    '14956791' => 'Arbind Thakur',
+    # 'faeid???' => 'Fae ??',
+  })
+end
