38984a40705f8a85115321d4733d732342cb8298
Arbind <arbind@carbonfive.com>
2013-09-21 19:47:54 -0700

    don't initialize redis in rake tasks, don't select redis DB in production

--- config/initializers/2_geocoder.rb
+++ config/initializers/2_geocoder.rb
@@ -1,9 +1,13 @@
-GEOCODER_CACHE_TTL = 86400 # (60s * 60m * 24h)
+unless $PROGRAM_NAME.end_with?('rake')
+  # Skip this initialization durring rake tasks
+  # Heroku runs rake asset:precompile in sandbox mode with no ENV or DBs
+  GEOCODER_CACHE_TTL = 86400 # (60s * 60m * 24h)
 
-geocoder_config = {
-  lookup: :google,
-  cache: RedisAutoExpire.new(REDIS4GEOCODER, GEOCODER_CACHE_TTL),
-  cache_prefix: "gO:" # gee-oooh :)
-}
+  geocoder_config = {
+    lookup: :google,
+    cache: RedisAutoExpire.new(REDIS4GEOCODER, GEOCODER_CACHE_TTL),
+    cache_prefix: "gO:" # gee-oooh :)
+  }
 
-Geocoder.configure geocoder_config
+  Geocoder.configure geocoder_config
+end
