17e8037660aaceb8fe7b406fdbe5a885e751024b
Arbind <arbind@carbonfive.com>
2013-09-21 20:12:34 -0700

    lazy load authorized users instance, instead of on class definition

--- config/initializers/3_authorized_users.rb
+++ config/initializers/3_authorized_users.rb
@@ -1,5 +1,5 @@
 # Add the twitter ids for any users you want to authorize
 AuthorizedUsers.service.authorize({
   '14956791' => 'Arbind Thakur',
-  'faeid???' => 'Fae ??',
+  # 'faeid???' => 'Fae ??',
 })
