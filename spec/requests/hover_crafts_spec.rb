19b2ebbb84312ed9eb705f7d29c41a78119b1573
Arbind Thakur <arbind.thakur@gmail.com>
2013-11-30 18:32:33 -0800

    comment out request spec until a mock oath login is in place

--- spec/requests/hover_crafts_spec.rb
+++ spec/requests/hover_crafts_spec.rb
@@ -1,11 +1,20 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "HoverCrafts" do
-  describe "GET /hover_crafts" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get hover_crafts_path
-      response.status.should be(200)
-    end
-  end
-end
+# describe "HoverCrafts" do
+#   let(:current_user) { u = create :twitter_user }
+
+#   def sign_in(u)
+#     # mock omniauth signin
+#     # controller.session.stub(:[]).with(:current_user).and_return(u.twitter_id)
+#   end
+
+#   describe "GET /hover_crafts" do
+#     before { sign_in current_user }
+
+#     it "works! (now write some real specs)" do
+#       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers      
+#       get hover_crafts_path
+#       response.status.should be(200)
+#     end
+#   end
+# end
